d7e43ea5973c9a9153b54c5531e8ca450ac166b7
kyeongwan <lk@lemonlab.co.kr>
2017-03-11 14:09:05 +0900

    Update Problem2.kt
    Add magic number for Bumjun
    Fix some bug

--- Problem2/Kotlin/kyeongwan/Problem2.kt
+++ Problem2/Kotlin/kyeongwan/Problem2.kt
@@ -1,15 +1,17 @@
-fun(arg: Array<String>){
 
-	var sum = 2
-	var s1 = 1
-	var s2 = 2
-	var currentVar = 0
+fun main(arg: Array<String>){
+    var sum = 2
+    var s1 = 1
+    var s2 = 2
+    var currentVar = 0
+    var range: Int = if(arg.size == 0) 4000000 else arg[1].toInt()
 
-	while(currentVar < 4000000){
-		currentVar = s1 + s2
-		if(currentVar%2 == 0) currentVar += sum
-			s1 = s2
-		s2 = currentVar
-	}
-	println(sum)
+    while(currentVar <= range){
+        currentVar = s1 + s2
+        if(currentVar.and(1) == 0)
+            sum += currentVar
+        s1 = s2
+        s2 = currentVar
+    }
+    println(sum)
 }
