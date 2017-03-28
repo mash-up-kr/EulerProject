971cc1eb200e1e31bdc476fc0808a0b7f8bb3164
mesoibum joon kim <omjoonkim@gmail.com>
2017-03-28 11:16:14 +0900

    refactor : run -> let in order() 진짜마지막 커밋...

--- Problem4/Kotlin/Omjoon/problem4.kt
+++ Problem4/Kotlin/Omjoon/problem4.kt
@@ -6,6 +6,6 @@ fun main(args: Array<String>) = answer()
 
 fun answer() = (100..999).mapNotNull(::order).max().run { println(this) }
 
-fun order(number : Int) : Int? = (100..999).reversed().find { (it * number).isPalindrome() }?.run { this * number }
+fun order(number : Int) : Int? = (100..999).reversed().find { (it * number).isPalindrome() }?.let { it * number }
 
 fun Int.isPalindrome() = toString().run { length % 2 == 0 && substring(0,length / 2) == substring(length / 2).reversed() }
