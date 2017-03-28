a515020b30f9dbbf88bb0391f89344c47695a7c9
mesoibum joon kim <omjoonkim@gmail.com>
2017-03-28 11:13:07 +0900

    refactor : 대칭수 조건식에 범용적인 조건 추가.

--- Problem4/Kotlin/Omjoon/problem4.kt
+++ Problem4/Kotlin/Omjoon/problem4.kt
@@ -8,4 +8,4 @@ fun answer() = (100..999).mapNotNull(::order).max().run { println(this) }
 
 fun order(number : Int) : Int? = (100..999).reversed().find { (it * number).isPalindrome() }?.run { this * number }
 
-fun Int.isPalindrome() = toString().run { substring(0,3) == substring(3).reversed() }
+fun Int.isPalindrome() = toString().run { length % 2 == 0 && substring(0,length / 2) == substring(length / 2).reversed() }
