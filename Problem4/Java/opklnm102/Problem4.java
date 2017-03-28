7661931dbd048edf2eac6e4e026fb13e185f2628
DongHee Kim <opklnm102@gmail.com>
2017-03-28 13:28:00 +0900

    Fix: modify solve problem4 with stream api

--- Problem4/Java/opklnm102/Problem4.java
+++ Problem4/Java/opklnm102/Problem4.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 /*
  * Largest palindrome product
  * 앞에서부터 읽을 때나 뒤에서부터 읽을 때나 모양이 같은 수를 대칭수(palindrome)라고 부릅니다.
@@ -6,21 +8,22 @@
  */
 public class Problem4 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int max = 0;
+        int max = IntStream.range(100, 1000)
+                .flatMap(x ->
+                        IntStream.range(100, 1000)
+                                .map(y -> x * y))
+                .filter(Problem4::isPalindrome)
+                .max().getAsInt();
 
-		for (int i = 100; i < 1000; i++) {
-			for (int j = 100; j < 1000; j++) {
-				int num = i * j;
-				StringBuilder sbNum = new StringBuilder(""+num);
-				sbNum.reverse();
-				
-				if(num == Integer.parseInt(sbNum.toString()) && num > max){
-					max = num;
-				}
-			}
-		}
-		System.out.println(max);
-	}
+        System.out.println(max);  // 906609
+    }
+
+    static boolean isPalindrome(int num) {
+        StringBuilder sbNum = new StringBuilder(String.valueOf(num));
+        String strReverseNum = sbNum.reverse().toString();
+
+        return num == Integer.parseInt(strReverseNum);
+    }
 }
